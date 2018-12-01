e27a7bec7026d43ed06da08a82d14b283916be6a
Ioan Morar <ioan.morar@iquestgroup.com>
2018-12-01 14:08:16 +0200

    remove bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
-import { BookList } from '../src/components/BookList/bookList';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
+import { BookList } from "../src/components/BookList/bookList";
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Rent a book</h1>
-        </header>
-        <BookList />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Rent a book</h1>
+				</header>
+				<div class="app-container">
+					<BookList />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/BookList/bookList.jsx
+++ src/components/BookList/bookList.jsx
@@ -7,18 +7,14 @@ export class BookList extends React.Component {
     render() {
         return (
         <div className="book-list-container">
-            <Col sm={6} md={3}>
-                <Book title="Amintiri din copilarie" author="Ion Creanga" rented={false}/>
-            </Col>
-            <Col sm={6} md={3}>
-                <Book title="Harap Alb" author="Ion Creanga" rented={true}/>
-            </Col>
-            <Col sm={6} md={3}>
-                <Book title="Fram ursul polar" author="Cezar Petrescu" rented={true}/>
-            </Col>
-            <Col sm={6} md={3}>
-                <Book title="Fabule" author="George Toparceanu" rented={false}/>
-            </Col>
+            <Book title="Trilogia Stapanul Inelelor" author="J.R.R. Tolkien" rented={false}/>
+            <Book title="Codul lui DaVinci" author="Dan Brown" rented={true}/>
+            <Book title="Hoțul de cărți" author="Markus Zusak" rented={true}/>
+            <Book title="Cronicile din Narnia – Leul" author="C.S.Lewis" rented={false}/>
+            <Book title="Harry Potter și piatra filozofală" author="J.K. Rowling" rented={true}/>
+            <Book title="Hobbitul" author="J.R.R. Tolkien" rented={false}/>
+            <Book title="Don Quixote" author="Miguel de Cervantes" rented={false}/>
+            <Book title="Moby Dick" author="Herman Melville" rented={true}/>
         </div>
         )
     }
